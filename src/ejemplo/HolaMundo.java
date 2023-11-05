3adbfb150bef6fdcb150ba0437cc0f61e7977728
Juana Moya <j.moya@edu.gva.es>
2023-11-05 09:03:36 +0100

    Primera versión

---
+++ src/ejemplo/HolaMundo.java
@@ -0,0 +1,10 @@
+package ejemplo;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class HolaMundo {
+    public static void main(String[] args)  {
+        System.out.println("Hola Mund
+    }
+}
